c3db2eee9b82abac429228e4480aafd2dc98fb0a
carlosapplyrodriguez <carlos.apply.rodriguez@gmail.com>
2023-02-25 21:22:17 -0700

    imind/nodejs-2 - Adding javascript exercises.

---
+++ print-to-file-example.js
@@ -0,0 +1,3 @@
+const fs = require('fs');
+
+fs.writeFileSync("filename.txt", "text to print in automatically created file in project's root directory, with name and filetype specified in previous parameter in this function.");
