242942378b0d2d8deb966343348534d4bf7d8a20
imindloop <imindloop@gmail.com>
2023-02-26 15:43:59 -0700

    imind/nodejs-5 - Getting request body, parsing chunks, buffering, then getting the actual data and finally saving to a file.

--- servers/application_routing.js
+++ servers/application_routing.js
@@ -22,13 +22,28 @@ const server = http.createServer( (request, response) => {
         return response.end();
     } else if (url === '/message' && method === 'POST') {
 
-        
+        const requestBody = [];
+        let parsedBody = '';
 
-        // We want to persist the message by saving it to a file.
-        fs.writeFileSync('message' + messageId+1 + '-' + new Date() + '.txt', );
-        response.statusCode = 302;
-        response.setHeader("location", "/");
-        return response.end();
+         // Ingesting the chunks of request body coming on the stream.
+        request.on('data', (chunk) => {
+            console.log('Request Body so far...' + chunk);
+            requestBody.push(chunk);
+        });
+        
+        // When we finally ingested all the chunks in the stream, we can buffer the requestBody.
+        request.on('end',() => {
+            parsedBody = Buffer.concat(requestBody).toString();
+            const message = parsedBody.split('=')[1];
+
+            // We want to persist the message by saving it to a file.
+            fs.writeFileSync('message' + messageId+1 + '-' + new Date() + '.txt', message);
+
+            response.statusCode = 302;
+            response.setHeader("location", "/");
+            return response.end();
+
+        });
     }
 });
 
