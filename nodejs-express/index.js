12523cfc74c3d91f455cd99408c7ef4eef3c6268
imindloop <imindloop@gmail.com>
2023-03-02 09:22:20 -0700

    imind/nodejs-12 - Adding third-party module body-parser.

--- nodejs-express/index.js
+++ nodejs-express/index.js
@@ -3,22 +3,32 @@ const http = require('http');
 
 // Third party imports
 const express = require('express');
+const bodyParser = require('body-parser');
 
 // Declaring the application from express
 const app = express();
 
-// Setting up the space for the middleware
-app.use('/add-user', (request, response, next) => {
-    response.send('The "add-user" page.');
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (request, response) => {
+    response.send(`<form action="/product" method="POST">
+                        <input type="text" name="productName">
+                        <button type="submit">Save</button>
+                    </form>`);
 });
 
-// Setting up the space for the middleware
+app.get('/product', (request, response, next) => {
+    console.log('We don\'t have a body because this was a GET request');
+    response.redirect('/');
+});
+
+app.post('/product', (request, response, next) => {
+    console.log(request.body);
+    response.redirect('/');
+});
+
 app.use('/', (request, response, next) => {
-    response.send('From a first function in the middleware realm!');
-});
-
-app.use( (request,  response, next) => {
-   response.send('<h1>Hello from Express Middleware.</h1>');
+    response.send('<h1>Welcome to the app</h1>');
 });
 
 // Creating the server using express, we don't need to call createServer()!
